6bc446292933cf31e8c2ce500171b8a1fb5cf0fe
Rohan-1-3 <rohanshrestha130@gmail.com>
2023-01-31 17:36:24 +0545

    added comments and capitalize 1st word function

--- src/modules/fetchData.js
+++ src/modules/fetchData.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-alert */
 export default async function getWeatherInfo(location){
-    
+    // fetching locations weather info
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=d02ff9591aed8d0a11368b857174ac68`,
     {mod : "cors"}
     )
     const locationData = await response.json();
-    if(locationData.cod !== 200){
+    if(locationData.cod !== 200){// condition for unavailable city
         alert("No such City or Country!");
         return false;
     }
--- src/modules/getLocation.js
+++ src/modules/getLocation.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 import getWeatherInfo from "./fetchData";
 
 const location = document.querySelector("#location-name");
@@ -10,31 +11,38 @@ const temp = document.querySelector("#temp");
 const feelsLike = document.querySelector("#feels-like");
 const windSpeed = document.querySelector("#wind");
 const minMax = document.querySelector("#min-max");
-location.value = "nepal";
+location.value = "nepal";// initial value for weather display
 const weatherFunction = ()=>{
     const weatherInfo = async ()=>{
-        if(location.value === ""){
+        if(location.value === ""){// condition for blank input
             alert("enter the name of city");
             return false;
         }
+        // fetching data from fetchData.js
         const weatherData = await getWeatherInfo(location.value);
-        console.log(weatherData);
-
-        description.textContent = weatherData.weather[0].description;
+        // capitalize 1st word
+        const descriptionCapital = weatherData.weather[0].description.split(" ");
+        const finalArray = [];
+        for(let i =0; i<descriptionCapital.length;i+=1){
+            finalArray[i] = descriptionCapital[i][0].toUpperCase() + descriptionCapital[i].substr(1).toLowerCase();
+        }
+        // inserting value in the HTML file
+        description.textContent = finalArray.join(" ");
         placeName.textContent = `${weatherData.name}, ${weatherData.sys.country}`;
         humidity.textContent = `Humidity: ${weatherData.main.humidity}`;
         temp.textContent = `${Math.round(weatherData.main.temp)}°C`;
-
         feelsLike.textContent = `Feels Like: ${weatherData.main.feels_like}°C`;
         windSpeed.textContent = `Wind Speed: ${weatherData.wind.speed} Km/hr`;
         minMax.textContent = `Temperature(Min/Max): ${weatherData.main.temp_min}°C/${weatherData.main.temp_max}°C`
+        // resetting input value
         location.value = "";
+        return 0;
     }
     
     submitButton.addEventListener("click", ()=>{
         weatherInfo();
     });
-
+    // enter key initiates the function
     location.addEventListener("keydown", (e)=>{
         if(e.keyCode === 13){
             weatherInfo();
